2f6fa99a374f65aaabe482bf5b1c454d58d507d4
siri <siree@siri.com>
2024-02-13 15:50:55 +0530

    first commit

---
+++ src/main/java/com/example/java8programs/Java8programsApplication.java
@@ -0,0 +1,13 @@
+package com.example.java8programs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8programsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8programsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java8programs/Java8programsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java8programs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8programsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
